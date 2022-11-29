fd9af0468f48a25424fe50e98470cd191897b1a8
Lope Ramos <lope.ramos@dataiku.com>
2022-11-29 09:51:52 +0100

    Create folder for 2022

---
+++ year_2022/ex_/main.py
@@ -0,0 +1,30 @@
+import logging
+from typing import Generator, Any
+
+from utils.debugging import debug
+from utils.runner import run_main
+
+logger = logging.getLogger(__name__)
+
+
+def pt_1(prob_input: Generator[str, Any, None]) -> int:
+    debug("testing debug")
+    return 0
+
+
+def pt_2(prob_input: Generator[str, Any, None]) -> int:
+    debug("testing debug")
+    return 0
+
+
+def main():
+    run_main(pt_1, pt_2, __file__, [
+        0,
+        0,
+        0,
+        0
+    ])
+
+
+if __name__ == "__main__":
+    main()
