396b9d98279a5bd5985f17a6294519ef0d833c21
Lope Ramos <loperamosmunoz@gmail.com>
2022-12-03 06:39:04 +0100

    day 3 of 2022 pretty

--- year_2022/ex_03/main.py
+++ year_2022/ex_03/main.py
@@ -2,54 +2,34 @@ import logging
 from string import ascii_lowercase, ascii_uppercase
 from typing import Generator
 
-from utils.debugging import debug, info
+from more_itertools import grouper
+
 from utils.runner import run_main
 
 logger = logging.getLogger(__name__)
 
-
-points_lower = {l: i + 1 for i, l in enumerate(ascii_lowercase)}
-points_upper = {l: i + 27 for i, l in enumerate(ascii_uppercase)}
-
-points = points_upper | points_lower
-debug(f"{points=}")
+points = {l: i + 1 for i, l in enumerate(ascii_lowercase)} | {l: i + 27 for i, l in enumerate(ascii_uppercase)}
 
 
 def pt_1(prob_input: Generator) -> int:
     p = 0
     for line in prob_input:
-        rs = list(line)
-        debug(rs)
-        size = len(rs)
-        debug(len(rs))
-        left, right = set(rs[0 : int(size / 2)]), set(rs[int(size / 2) :])
-        debug(f"{left=}, {right=}")
-        for c in left:
-            if c in right:
-                debug(c)
-                p += points[c]
-
+        rs_size = len(line) // 2
+        same = set(line[0:rs_size]) & set(line[rs_size:])
+        p += points[same.pop()]
     return p
 
 
 def pt_2(prob_input: Generator) -> int:
     p = 0
-    lines = [set(l) for l in prob_input]
-    n_rs = int(len(lines) / 3)
-    for i in range(n_rs):
-        a = lines[i * 3]
-        b = lines[i * 3 + 1]
-        c = lines[i * 3 + 2]
-        info(f"{a=}, {b=}, {c=}")
-        same = a & b & c
-        info(f"{same=}")
+    for a, b, c in grouper(prob_input, 3):
+        same = set(a) & set(b) & set(c)
         p += points[same.pop()]
-
     return p
 
 
 def main():
-    run_main(pt_1, pt_2, __file__, [157, 7568, 70, 0])
+    run_main(pt_1, pt_2, __file__, [157, 7568, 70, 2780])
 
 
 if __name__ == "__main__":
